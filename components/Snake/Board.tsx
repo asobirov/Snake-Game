554e4d62cc0e064b910b69540de1ec434ac04ca2
asobirov <asobirov357@gmail.com>
2021-11-10 20:00:54 +0000

    Add Moving snake (1 render behind)

--- components/Snake/Board.tsx
+++ components/Snake/Board.tsx
@@ -1,14 +1,125 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { Grid } from "@chakra-ui/react"
 import Cell, { TBlockTypes } from "./Cell"
 
+type Coordinates = {
+    x: number,
+    y: number
+}
+type Direction = 'up' | 'down' | 'left' | 'right';
+
 const Board = () => {
-    const [size, setSize] = useState<number>(50)
-    const [board, setBoard] = useState<TBlockTypes[][]>(() => Array.from({ length: size }, () => Array.from({ length: size }, () => "blank")));
+    const [size, setSize] = useState<number>(50);
+    const [speed, setSpeed] = useState<number>(200);
 
-    const displaySnake = () => {
-
+    const getRandomCoordinates = (max: number = size - 1): Coordinates => {
+        let min = Math.ceil(0);
+        max = Math.floor(max);
+        return {
+            x: Math.floor(Math.random() * (max - min + 1) + min),
+            y: Math.floor(Math.random() * (max - min + 1) + min)
+        }
     }
+    const [food, setFood] = useState<Coordinates>(getRandomCoordinates)
+
+    const initBoard = () => {
+        const _board: TBlockTypes[][] = Array.from({ length: size }, () => Array.from({ length: size }, () => "blank"));
+
+        const snake = [{ x: 0, y: 0 }, { x: 1, y: 0 }];
+
+        _board[food.x][food.y] = 'food'
+        snake.forEach(cell => {
+            _board[cell.x][cell.y] = 'snake'
+        })
+
+        return _board
+    }
+    const [board, setBoard] = useState<TBlockTypes[][]>(initBoard);
+
+    const [snake, setSnake] = useState<Coordinates[]>(() => [{ x: 0, y: 0 }, { x: 1, y: 0 }]);
+    const [direction, setDirection] = useState<Direction>('right');
+
+    const handleKeyDown = (e: any) => {
+        const key = e.keyCode;
+
+        switch (key) {
+
+            case 40:
+                setDirection('down');
+                break;
+            case 39:
+                setDirection('right');
+                break;
+            case 38:
+                setDirection('up');
+                break;
+            case 37:
+                setDirection('left');
+                break;
+
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('keydown', handleKeyDown);
+    }, [])
+
+    const moveSnake = () => {
+        const _snake: Coordinates[] = [...snake];
+        let head = _snake[_snake.length - 1];
+
+        switch (direction) {
+            case 'right':
+                head = { x: head.x + 1, y: head.y };
+                break;
+
+            case 'left':
+                head = { x: head.x - 1, y: head.y }
+                break;
+
+            case 'up':
+                head = { x: head.x, y: head.y - 1 }
+                break;
+
+            case 'down':
+                head = { x: head.x, y: head.y + 1 }
+                break;
+
+        }
+        _snake.push(head);
+        const tail = _snake.shift()!;
+        setSnake(_snake);
+
+        const _board = [...board];
+        _board[tail.x][tail.y] = 'blank'
+        setBoard(_board);
+    }
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            moveSnake();
+        }, speed);
+        return () => clearInterval(interval)
+    }, [snake])
+
+    const updateBoard = ({ snake, food }: { snake?: Coordinates[], food?: Coordinates }) => {
+        const _board = board;
+
+        if (food) {
+            _board[food.x][food.y] = 'food'
+        }
+
+        if (snake) {
+            snake.forEach(cell => {
+                _board[cell.x][cell.y] = 'snake'
+            })
+        }
+        return _board;
+    }
+
+    useEffect(() => {
+        setBoard(updateBoard({ snake }));
+    }, [snake])
     return (
         <Grid
             templateColumns={`repeat(${size}, 1fr)`}
@@ -18,10 +129,11 @@ const Board = () => {
             border='1px'
             w='min-content'
             borderColor='whiteAlpha.700'
+            gridAutoFlow='column'
         >
-            {board.map(row => {
-                return row.map((cell, key) => (
-                    <Cell key={key} type={cell} />
+            {board.map((column, rK) => {
+                return column.map((cell, key) => (
+                    <Cell key={key} idx={`${rK}:${key}`} type={cell} />
                 ))
             })}
         </Grid>
--- components/Snake/Cell.tsx
+++ components/Snake/Cell.tsx
@@ -3,12 +3,13 @@ import { Box } from "@chakra-ui/react"
 export type TBlockTypes = 'snake' | 'food' | 'blank';
 
 type BlockProps = {
-    type: TBlockTypes
+    type: TBlockTypes,
+    idx: any
 }
 
-const Block = ({ type }: BlockProps) => {
+const Block = ({ type, idx }: BlockProps) => {
     return (
-        <Box w='10px' h='10px' bg={type === 'snake' ? 'gray.500' : type === 'food' ? 'orange' : 'black'} />
+        <Box w='10px' h='10px' data-coords={idx} bg={type === 'snake' ? 'gray.500' : type === 'food' ? 'orange' : 'black'} datatype={type} />
     )
 }
 
