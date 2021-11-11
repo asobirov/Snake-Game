08908cda636056b2d6edcbd096fbb4af08194571
asobirov <asobirov357@gmail.com>
2021-11-11 06:09:25 +0000

    Minor Changes

--- lib/redux/slices/foodSlice.ts
+++ lib/redux/slices/foodSlice.ts
@@ -22,12 +22,9 @@ const foodSlice = createSlice({
   initialState,
   reducers: {
     spawnFood(state, action: PayloadAction<TSpawFood>) {
-      let min = Math.ceil(0);
-      let max = Math.floor(action.payload.max);
-      state = {
-        x: Math.floor(Math.random() * (max - min + 1) + min),
-        y: Math.floor(Math.random() * (max - min + 1) + min),
-      };
+      const { max } = action.payload;
+      state.x = Math.floor(Math.random() * max);
+      state.y = Math.floor(Math.random() * max);
     },
   },
 });
